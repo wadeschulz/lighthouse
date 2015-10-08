fb424cf939f824967f9196d1bd97df06e8d37f64
Wade Schulz, MD, PhD <wade.schulz@gmail.com>
2015-10-08 08:48:54 -0400

    add initial app

---
+++ lighthouse.app/lighthouse/views.py
@@ -0,0 +1,26 @@
+﻿"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from lighthouse import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.jade',
+        title='Typhoon - Lighthouse',
+        year=2015
+    )
+
+@app.route('/annotate')
+def annotate():
+    """Renders the annotation page."""
+    return render_template(
+        'annotate.jade',
+        title='Annotate',
+        year=2015
+    )
