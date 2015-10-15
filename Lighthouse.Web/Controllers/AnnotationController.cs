77888931918259c96c2d538d699403573df66321
Wade Schulz, MD, PhD <wade.schulz@gmail.com>
2015-10-15 15:47:50 -0400

    start of annotation models

---
+++ Lighthouse.Web/Controllers/AnnotationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lighthouse.Web.Controllers
+{
+    public class AnnotationController : Controller
+    {
+        // GET: Annotate
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateAnnotation model)
+        {
+            
+        }
+    }
+}
---
+++ Lighthouse.Web/Models/AnnotationModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Lighthouse.Web.Models
+{
+    public class CreateAnnotationModel
+    {
+        public string Panel { get; set; }
+        public string CaseId { get; set; }
+        public HttpPostedFileBase VcfFile { get; set; }
+    }
+
+    public class AnnotationResultModel
+    {
+        public string CaseId { get; set; }
+        public string Panel { get; set; }
+        public IList<string> CaseAnnotations { get; set; }
+        public IList<VariantAnnotationModel> VariantAnnotationModels { get; set; } 
+    }
+
+    public class VariantAnnotationModel
+    {
+        public string Chromosome { get; set; }
+        public string Location { get; set; }
+        public IList<string> VariantAnnotations { get; set; } 
+    }
+}
